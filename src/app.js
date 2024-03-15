204144b86515405a2a2be1558ad863da49d6c696
samir suroshe <sameersuroshe50@gmail.com>
2024-03-15 22:15:02 +0530

    changed in error handling middleware

--- src/app.js
+++ src/app.js
@@ -34,9 +34,11 @@ app.use("/api/v1/usertaskinfo", userTaskInfoRouter);
 
 
 app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
     
-      return res.status(err.statusCode).json({
-        message : err.message,
+      return res.status(statusCode).json({
+        message : message
       });
     
 })
