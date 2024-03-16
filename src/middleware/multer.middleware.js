d5a3cd2a7d69cdcf4721a397a1f177754e4a6b22
samir suroshe <sameersuroshe50@gmail.com>
2024-03-16 14:07:54 +0530

    did some changes in multer middleware

--- src/middleware/multer.middleware.js
+++ src/middleware/multer.middleware.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, path.join(process.cwd(), '../../public/temp'));
+      cb(null, path.join(__dirname, '/public/temp'));
     },
     filename: function (req, file, cb) {
       cb(null, file.originalname)
@@ -12,4 +12,3 @@ const storage = multer.diskStorage({
   })
   
   export const upload = multer({ storage });
-  // export const upload = multer({ storage : storage });
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,29 +1,3 @@
-// import { v2 as cloudinary } from 'cloudinary';
-// import fs from 'fs'
-
-// cloudinary.config({
-// cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-// api_key: process.env.CLOUDINARY_API_KEY,
-// api_secret: process.env.CLOUDINARY_API_SECRET,
-// secure: true
-// });
-
-// const uploadOnCloudinary = async (localFilePath) => {
-// try {
-//     console.log(localFilePath);
-//     const response = await cloudinary.uploader.upload(localFilePath);
-//     console.log("file is uploaded on cloudinary sdk : ",response.url);
-//     return response;
-// }
-// catch (err) {
-//     fs.unlinkSync(localFilePath)//remove the locally saved temporary files as the upload operation got failed
-//     return null;
-// }
-
-// }
-
-// export default uploadOnCloudinary
-
 import { v2 as cloud } from "cloudinary";
 import fs from 'fs'
 
@@ -36,15 +10,14 @@ cloud.config({
 const uploadOnCloudinary = async (localFilePath) => {
     try {
 
-        if (!fs.existsSync(localFilePath)) {
-            console.log(`File does not exist at path: ${localFilePath}`);
-            return null;
-        }
+        if (!localFilePath) return null;
 
         const response = await cloud.uploader.upload(localFilePath, { resource_type: "auto" });
         console.log("file is uploaded on cloudinary sdk : ", response.url);
 
-        fs.unlinkSync(localFilePath)//remove the locally saved temporary files as the upload operation got successfull
+        if (fs.existsSync(localFilePath)) {
+            fs.unlinkSync(localFilePath)//remove the locally saved temporary files as the upload operation got successfull
+        }
         
         return response;
     }
