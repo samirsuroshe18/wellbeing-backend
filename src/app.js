88a68f4eb97458bea4c836f4ef082f45182f8bce
samir suroshe <sameersuroshe50@gmail.com>
2024-03-16 17:33:35 +0530

    app.js file changed

--- src/app.js
+++ src/app.js
@@ -13,8 +13,8 @@ app.use(express.urlencoded({extended: true}))
 // this is used for accessing public resources from server
 // app.use(express.static("public"));
 
-app.use(express.static(path.join(new URL('.', import.meta.url).pathname, 'public/temp')));
-app.use('/public/temp', express.static('public/temp'));
+app.use(express.static(path.join(new URL('.', import.meta.url).pathname, 'public/tmp')));
+app.use('/public/tmp', express.static('public/tmp'));
 
 
 //Routes import
