3fbf7df37c079b9cf53ee34d190c3e575b8b7f70
samir suroshe <sameersuroshe50@gmail.com>
2024-03-16 16:16:27 +0530

    store in absolute path

--- src/middleware/multer.middleware.js
+++ src/middleware/multer.middleware.js
@@ -1,17 +1,9 @@
 import multer from "multer";
 import path from "path";
 
-// Get the absolute path to the destination directory
-const destinationPath = path.resolve(process.cwd(), 'src', 'public', 'temp');
-
-// Ensure that the directory exists, create it if it doesn't
-if (!fs.existsSync(destinationPath)) {
-    fs.mkdirSync(destinationPath, { recursive: true });
-}
-
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, destinationPath);
+      cb(null, path.join(process.cwd(), 'public', 'temp'));
     },
     filename: function (req, file, cb) {
       cb(null, file.originalname)
