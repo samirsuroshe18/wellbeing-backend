c02063e1f1fd20fbccdeab5fb4bdf44fab8c400f
samir suroshe <sameersuroshe50@gmail.com>
2024-03-16 14:21:44 +0530

    changes in multer

--- src/middleware/multer.middleware.js
+++ src/middleware/multer.middleware.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, path.join(__dirname, '/public/temp'));
+      cb(null, path.join(process.cwd(), '/public/temp'));
     },
     filename: function (req, file, cb) {
       cb(null, file.originalname)
