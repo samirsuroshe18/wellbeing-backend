a52aa4327adc9e3e64e0c3b00e1f4336c5737ffa
samir suroshe <sameersuroshe50@gmail.com>
2024-03-16 14:16:31 +0530

    did changes in app.js

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended: true}))
 // this is used for accessing public resources from server
 // app.use(express.static("public"))
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const __dirname = path.dirname("public");
 app.use(express.static(__dirname));
 
 
