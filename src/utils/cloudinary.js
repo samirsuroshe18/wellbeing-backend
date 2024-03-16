87c5ca348d7cbe403adb830353b1097bbaf888c6
samir suroshe <sameersuroshe50@gmail.com>
2024-03-16 17:48:35 +0530

    add temp folder

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,29 +1,3 @@
-// import { v2 as cloudinary } from 'cloudinary';
-// import fs from 'fs'
-
-// cloudinary.config({
-// cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-// api_key: process.env.CLOUDINARY_API_KEY,
-// api_secret: process.env.CLOUDINARY_API_SECRET,
-// secure: true
-// });
-
-// const uploadOnCloudinary = async (localFilePath) => {
-// try {
-//     console.log(localFilePath);
-//     const response = await cloudinary.uploader.upload(localFilePath);
-//     console.log("file is uploaded on cloudinary sdk : ",response.url);
-//     return response;
-// }
-// catch (err) {
-//     fs.unlinkSync(localFilePath)//remove the locally saved temporary files as the upload operation got failed
-//     return null;
-// }
-
-// }
-
-// export default uploadOnCloudinary
-
 import { v2 as cloud } from "cloudinary";
 import fs from 'fs'
 
